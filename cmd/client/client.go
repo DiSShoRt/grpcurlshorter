c2627a7aee2b3219b93021c353594f67da632728
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-11 19:06:43 +0300

    Add files via upload

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpcurlshorter/pkg/urlshorter"
+)
+cd
+func main() {
+
+	var x string
+	fmt.Println("please, input url")
+
+	fmt.Scan(&x)
+
+	conn, _ := grpc.Dial(":8080", grpc.WithInsecure())
+	c := urlshorter.NewUrluhorterClient(conn)
+
+	r ,_ :=c.Create(context.Background(), &urlshorter.LongUrl{Long: x})
+	res, _ :=c.Get(context.Background(), &urlshorter.ShortUrl{Short:r.Short})
+
+	fmt.Println(res)
+	fmt.Println(r)
+}
