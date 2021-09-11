14cb87986ba4a728380f9eaf5d5c4fc40e20e166
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-11 19:05:38 +0300

    Add files via upload

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,40 @@
+package main
 
+import (
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	urlshort "grpcurlshorter/pkg/interf"
+
+	"grpcurlshorter/pkg/urlshorter"
+
+	"log"
+
+	"net"
+)
+
+func main()  {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("env")
+	}
+
+	if err := InitCinfig(); err != nil {
+		log.Fatal("error init config", err)
+	}
+	s := grpc.NewServer()
+	srv := &urlshort.GRPCServer{}
+	urlshorter.RegisterUrluhorterServer(s,srv)
+	l,_ := net.Listen("tcp",":8080")
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+
+func InitCinfig() error {
+		viper.AddConfigPath("configs")
+		viper.SetConfigName("config")
+
+		return viper.ReadInConfig()
+}
