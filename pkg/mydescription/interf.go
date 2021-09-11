e9238caa43c2dc48ce2cc93cfc0de401059b5d80
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-11 19:43:24 +0300

    Add files via upload

--- pkg/mydescription/interf.go
+++ pkg/mydescription/interf.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"fmt"
 	"grpcurlshorter/pkg/urlshorter"
-	"grpcurlshorter/store"
+	"grpcurlshorter/storage"
+
 	"log"
 )
 
-type GRPCServer struct {}
+type GRPCServer struct{}
 
 func (s *GRPCServer) Create(ctx context.Context, lurl *urlshorter.LongUrl) (*urlshorter.ShortUrl, error) {
-	short, err := store.AddUrlToDb(lurl.Long)
+	short, err := storage.AddUrlToDb(lurl.Long)
 	if err != nil {
 		log.Println(err)
 		return &urlshorter.ShortUrl{Short: short}, err
@@ -20,12 +21,11 @@ func (s *GRPCServer) Create(ctx context.Context, lurl *urlshorter.LongUrl) (*url
 	return &urlshorter.ShortUrl{Short: short}, nil
 }
 
-func  (s *GRPCServer) Get(ctx context.Context, surl *urlshorter.ShortUrl) (*urlshorter.LongUrl, error) {
-	long, err := store.GetUrlFromDb(surl.Short)
-	 if err !=  nil {
-		 fmt.Println(err)
-		 return &urlshorter.LongUrl{Long: long }, err
-	 }
-	return &urlshorter.LongUrl{Long: long }, nil
+func (s *GRPCServer) Get(ctx context.Context, surl *urlshorter.ShortUrl) (*urlshorter.LongUrl, error) {
+	long, err := storage.GetUrlFromDb(surl.Short)
+	if err != nil {
+		fmt.Println(err)
+		return &urlshorter.LongUrl{Long: long}, err
+	}
+	return &urlshorter.LongUrl{Long: long}, nil
 }
-
