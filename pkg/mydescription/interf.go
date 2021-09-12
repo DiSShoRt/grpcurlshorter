ab6199a992a0ed3648494d5827ebdab5921d8b25
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-12 22:51:49 +0300

    Update interf.go

--- pkg/mydescription/interf.go
+++ pkg/mydescription/interf.go
@@ -15,7 +15,7 @@ func (s *GRPCServer) Create(ctx context.Context, lurl *urlshorter.LongUrl) (*url
 	short, err := storage.AddUrlToDb(lurl.Long)
 	if err != nil {
 		log.Println(err)
-		return &urlshorter.ShortUrl{Short: short}, err
+		return &urlshorter.ShortUrl{}, err
 	}
 
 	return &urlshorter.ShortUrl{Short: short}, nil
@@ -25,7 +25,8 @@ func (s *GRPCServer) Get(ctx context.Context, surl *urlshorter.ShortUrl) (*urlsh
 	long, err := storage.GetUrlFromDb(surl.Short)
 	if err != nil {
 		fmt.Println(err)
-		return &urlshorter.LongUrl{Long: long}, err
+		return &urlshorter.LongUrl{}, err
 	}
 	return &urlshorter.LongUrl{Long: long}, nil
 }
+
