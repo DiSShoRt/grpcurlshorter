cc35976b5c7b0cd820c3960eab162e581ac052ab
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-12 20:43:39 +0300

    Add files via upload

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,34 @@
+package main
 
+import (
+	
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	urlshort "grpcurlshorter/pkg/mydescription"
+
+	"grpcurlshorter/pkg/urlshorter"
+
+	"log"
+
+	"net"
+)
+
+func main()  {
+
+	s := grpc.NewServer()
+	srv := &urlshort.GRPCServer{}
+	urlshorter.RegisterUrluhorterServer(s,srv)
+	l,_ := net.Listen("tcp",":8080")
+	if err := s.Serve(l); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+
+func InitCinfig() error {
+		viper.AddConfigPath("configs")
+		viper.SetConfigName("config")
+
+		return viper.ReadInConfig()
+}
