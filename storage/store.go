8146aaedf24337216f430e2424fe91399dfbc450
DiSShoRt <74114111+DiSShoRt@users.noreply.github.com>
2021-09-12 22:50:19 +0300

    Update store.go

--- storage/store.go
+++ storage/store.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	
 	"fmt"
 	"github.com/jackc/pgx"
 	"log"
@@ -9,49 +8,55 @@ import (
 	u "net/url"
 	"strings"
 	"time"
-
 )
 
+type UserModele struct {
+	Surl string
+	Lurl string
+}
 
-//connStr := "user=anton password=123 dbname=postgres sslmode=disable"
+const connStr = "user=anton password=123 dbname=postgres sslmode=disable"
 
 func AddUrlToDb(s string) (string, error) {
+	err := Check(s)
+	if err != nil {
+		log.Fatalf("this url %s is in the database\n", s)
+		return s, fmt.Errorf("this url %s is in the database\n", s)
+		panic(err)
+	}
 
 	if s == "" {
-		return "", fmt.Errorf("empty string in request")
+		return "", fmt.Errorf("empty string in request\n")
 	}
 
 	if !IsUrl(s) {
-		return s, fmt.Errorf("no valid url")
+		return s, fmt.Errorf("no valid url\n")
 	}
 
-	connStr := "user=anton password=123 dbname=postgres sslmode=disable"
+	fmt.Println(err)
 
 	conf, err := pgx.ParseConnectionString(connStr)
 
 	//connect to db
 	db, err := pgx.Connect(conf)
 	if err != nil {
-		log.Println("cant connect to db %s", err)
+		log.Printf("cant connect to db %s", err)
 	}
 	// close connection
 	defer db.Close()
 	var shortUrl string
 	shortUrl = GetHash()
-	//fmt.Println(shortUrl)
+
 	var surl string
-	err = db.QueryRow("INSERT INTO url VALUES ( $1, $2) returning short_url", shortUrl, s).Scan(&surl)
 
 	if err != nil {
-		log.Println("cant connect to db %s", err)
+		err = db.QueryRow("INSERT INTO url VALUES ( $1, $2) returning short_url", shortUrl, s).Scan(&surl)
+	}
+	if err != nil {
+		log.Printf("cant connect to db %s", err)
 	}
 	return surl, nil
 
-}
-
-type UserModele struct {
-	Surl string
-	Lurl string
 }
 
 func GetUrlFromDb(s string) (string, error) {
@@ -62,13 +67,10 @@ func GetUrlFromDb(s string) (string, error) {
 	}
 
 	if len(s) != 10 {
-		log.Println("no valid")
-		return s, fmt.Errorf("no valid short url")
+		log.Printf("no valid url %s", s)
+		return s, fmt.Errorf("no valid short url %s", s)
 	}
 
-	//create config string
-	connStr := "user=anton password=123 dbname=postgres sslmode=disable"
-	//create connect config
 	conf, err := pgx.ParseConnectionString(connStr)
 
 	//connect to db
@@ -78,27 +80,22 @@ func GetUrlFromDb(s string) (string, error) {
 	}
 
 	defer db.Close()
-	//one row
+
 	var url string
-	//var rez grpcResp
+
 	err = db.QueryRow("SELECT long_url FROM url WHERE short_url = $1", s).Scan(&url)
-
-	//defer post.Close()
-	//walk to posts
-
-	//fmt.Println(url)
 	if err != nil {
 		log.Println(err)
 		return url, fmt.Errorf("connect to db")
 	}
-	//fmt.Println(p)
+
 	return url, nil
 }
 
 func GetHash() string {
 	rand.Seed(time.Now().Unix())
 	letter := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_1234567890")
-	
+
 	var b strings.Builder
 	for i := 0; i < 10; i++ {
 		b.WriteRune(letter[rand.Intn(len(letter))])
@@ -110,8 +107,40 @@ func IsUrl(url string) bool {
 	_, err := u.ParseRequestURI(url)
 
 	if err != nil {
-		log.Println("url", err)
+		log.Println("no valid url", err)
 		return false
 	}
 	return true
+}
+
+func Check(s string) error {
+
+	//create connect config
+	conf, err := pgx.ParseConnectionString(connStr)
+
+	//connect to db
+	db, err := pgx.Connect(conf)
+	if err != nil {
+		log.Printf("cant connect to db %s\n", err)
+	}
+
+	defer db.Close()
+	//one row
+
+	rows, err := db.Query("SELECT * FROM url WHERE long_url = $1", s)
+
+	for rows.Next() {
+		p := UserModele{}
+		err := rows.Scan(&p.Lurl, &p.Surl)
+		fmt.Println(p)
+		if s == p.Lurl || s == p.Surl {
+			fmt.Println(err)
+			fmt.Printf("this url %s is in database\n", s)
+			return fmt.Errorf("this url %s is in database\n", s)
+
+		}
+
+	}
+
+	return nil
 }
